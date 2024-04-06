97865b5a4df1087c92dc752f385bf8a06c382926
frypan05 <daksharma5804@gmail.com>
2024-04-06 15:06:17 +0530

    hardhat, ether, web3modal

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return     <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default index;
